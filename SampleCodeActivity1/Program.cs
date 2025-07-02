fc7c28a4eaa6f0197d92738cd9e4788e48d51e24
jeloFojas <jelo.fojas@deped.gov.ph>
2025-07-02 14:37:45 +0800

    Add project files.

---
+++ SampleCodeActivity1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleCodeActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			string name = "RJ";
+			int age = 30;
+			string section = "Grade 11 - ICT B";
+			double genAveGr10 = 84;
+
+			Console.WriteLine($"Hi, I am {name}, {section}. I am {age} years old with a Grade 10 General Average of {genAveGr10}.");
+		}
+    }
+}
